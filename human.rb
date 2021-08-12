b2ecdf02a9b6c817768d94f2caa5f1ea00b69621
yuru.secret <yuru.secret@gmail.com>
2021-08-12 14:03:37 +0000

    First Commit

---
+++ human.rb
@@ -0,0 +1,19 @@
+# < を使って、Animal クラスを継承する記述を入れてください
+# Thinkable モジュールの think メソッドを、このクラスで使えるようにしたいので、Thinkable モジュールを include してください
+# attr_accessor を使って、趣味の情報を保管する変数を定義してください。変数名は自由とします
+# initializeに名前・年齢・趣味の情報を格納する引数を設定し、処理内容として名前・年齢・趣味の各変数を引数の情報で初期化するようにしてください。引数の名前は自由とします
+
+require './animal'
+require './thinkable'
+
+
+class Human < Animal
+    include Thinkable
+    attr_accessor :hobby
+    
+    def initialize(name, old, hobby)
+      super(name, old)
+      self.hobby = hobby
+    end
+
+end
